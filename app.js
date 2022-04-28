778da92aa5bc1d0224b08d322b1d4f668498d683
Matthew Wang <matwan@bergen.org>
2022-04-28 16:48:10 -0400

    Step 4.1- 4.3.2.2 - (req->query->data->res w/ ejs)

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const logger = require( "morgan" );
 const app = express();
 const port = 8080;
 
+//Configure Express to use EJS
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+const db = require('./db/db_connection');
+
 //defining middleware that logs all incoming requests.
 app.use(logger("dev"));
 
@@ -12,17 +18,48 @@ app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
+    res.render('index');
 } );
+
+const read_stuff_all_sql = `
+    SELECT
+        id, item, quantity
+    FROM
+        stuff
+`
 
 // define a route for the stuff inventory page
 app.get( "/stuff", ( req, res ) => {
-    res.sendFile( __dirname + "/views/stuff.html" );
+    db.execute(read_stuff_all_sql, (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else 
+            res.send(results);
+    })
 } );
 
+const read_stuff_item_sql = `
+    SELECT
+        id, item, quantity, description
+    FROM
+        stuff
+    WHERE
+        id = ?
+`
+
 // define a route for the item detail page
-app.get( "/stuff/item", ( req, res ) => {
-    res.sendFile( __dirname + "/views/item.html" );
+app.get( "/stuff/item/:id", ( req, res ) => {
+    db.execute(read_stuff_item_sql, [req.params.id], (error, results) =>{
+        if(error)
+            res.status(500).send(error); //Internal Server Error
+        else if (results.length == 0)
+            res.status(404).send(`No item found with id = ${req.params.id}`); // NOT FOUND
+        else {
+            let data = results[0]; // results is still an array
+            //{ item: ___ , quantity:___, description: ___}
+            res.render('item', data)
+        }
+    })
 } );
 
 
